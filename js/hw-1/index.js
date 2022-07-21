371d7cc41329517ebc2ec1fa6ca4994786eb7a76
KedaSolya <solya.keda@gmail.com>
2022-07-21 22:32:14 +0300

    js hw1

---
+++ js/hw-1/index.js
@@ -0,0 +1,24 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+// let arr = ['hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false]
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+//     alert(arr[i]);
+//     document.writeln("<p>" + arr[i] + "</p>");
+// }
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+// let firstName = 'Solomiya', middleName = 'Volodymyrivna', lastName = 'Keda'
+// let person = firstName + ' ' + middleName + ' ' + lastName;
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+// let a = 100; let b = '100'; let c = true;
+// console.log(typeof a)
+// console.log(typeof b)
+// console.log(typeof c)
+
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+// let firstName = prompt('Your first name:')
+// let middleName = prompt('Your middle name:')
+// let age = prompt('Your age:')
+// console.log(firstName + ' ' + middleName + ', ' + age + ' years old')
