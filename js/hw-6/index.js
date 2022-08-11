4b8b20c9460925465771420c90c92d0628a7e223
KedaSolya <solya.keda@gmail.com>
2022-08-11 16:48:51 +0300

    js hw6 part1

---
+++ js/hw-6/index.js
@@ -0,0 +1,86 @@
+// - Знайти та вивести довижину настипних стрінгових значень
+// 'hello world', 'lorem ipsum', 'javascript is cool'
+// function length(str) {
+//     return str.length
+// }
+
+// - Перевести до великого регістру наступні стрінгові значення
+// 'hello world', 'lorem ipsum', 'javascript is cool'
+// function upperCase(str) {
+//     return str.toUpperCase()
+// }
+
+// - Перевести до нижнього регістру настипні стрінгові значення
+// 'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
+// function lowerCase(str) {
+//     return str.toLowerCase()
+// }
+
+// - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
+// let str = ' dirty string   '
+// str = str.trim()
+
+// - Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
+// let str = 'Ревуть воли як ясла повні';
+// let arr = stringToarray(str); ['Ревуть', 'воли', 'як', 'ясла', 'повні']
+// function stringToarray(str){
+//     return str.split(' ')
+// }
+
+// - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map та колбеку перетворити всі об'єкти в масиві на стрінгові.
+// let array = [10, 8, -7, 55, 987, -1011, 0, 1050, 0]
+// let str = array.map(elem => elem.toString())
+
+// - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
+// let nums = [11,21,3];
+// sortNums(nums,'ascending') // [3,11,21]
+// sortNums(nums,'descending') // [21,11,3]
+// function sortNums(nums, direction){
+//     if (direction === "ascending"){
+//         console.log(nums.sort(function (a,b){
+//             return a - b
+//         }))
+//     }
+//     if (direction === "descending"){
+//         console.log(nums.sort(function (a,b){
+//             return b - a
+//         }))
+//     }
+// }
+
+// - є масив
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// -- відсортувати його за спаданням за monthDuration
+
+// -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+//
+// описати колоду карт
+// - знайти піковий туз
+// - всі шістки
+// - всі червоні карти
+// - всі буби
+// - всі трефи від 9 та більше
+//
+//
+// {
+//     cardSuit: '', // 'spade', 'diamond','heart', 'clubs'
+//     value: '', // '6'-'10', 'ace','jack','queen','king','joker'
+//     color:'', // 'red','black'
+// }
+//
+//
+// Додатково по reduce
+// Взяти описану колоду карт, та за допомоги редюсу попакувати всі карти по "мастях" в об'єкт
+// {
+//     spades:[],
+//     diamonds:[],
+//     hearts:[],
+//     clubs:[]
+// }
